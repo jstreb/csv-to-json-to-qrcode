4154484d679445be9c7be8630ba28e1b4be13cc7
Jesse Streb <jesse.streb@gmail.com>
2012-05-23 07:04:15 -0400

    added a readme, made the script more dynamic so that is uses the columns from the csv file to generate the JSON properties

--- parse.js
+++ parse.js
@@ -2,7 +2,14 @@ var fs = require('fs');
 var sys = require('util');
 var QRCode = require('qrcode');
 
-var stream = fs.createReadStream("users.csv");
+var csvFile = process.argv[2] || "users.csv";
+
+if( csvFile.substring( csvFile.length - 3, csvFile.length) !== "csv" ) {
+  console.log( "The script only accepts a csv file a valid source file.");
+  return;  
+}
+
+var stream = fs.createReadStream( csvFile );
 var buffer = "";
 
 function generateAndSaveQRCode( text, fileName ) {
@@ -13,8 +20,14 @@ function generateAndSaveQRCode( text, fileName ) {
 
 stream.addListener('data', function(data){
   var contacts;
+  var topics;
+  
   buffer += data.toString();
   contacts = buffer.split('\r')
+  
+  //Get the top level categories
+  topics = contacts[0].split( "," );
+  
   //move the first contact off as this is the headers..."first name", "title"
   contacts.shift();
   
@@ -24,13 +37,14 @@ stream.addListener('data', function(data){
     var url;
     var fileName;
     
-    obj.firstName = info[0];
-    obj.lastName = info[1];
-    obj.company = info[2];
-    obj.title = info[3];
-    obj.email = info[4];
+    for( var j=0; j<topics.length; j++) {
+      if( info[j] === undefined || topics[j] === "" ) {
+        break;
+      }
+      obj[topics[j]] = info[j].replace( "\n", "" );
+    }
     
-    fileName = "./output/" + info[0] + info[1] + ".jpg";
+    fileName = "./output/" + info[0].replace( "\n", "" ) + ".jpg";
     
     obj = JSON.stringify( obj );
     
